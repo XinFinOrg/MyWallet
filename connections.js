d488366ecc1657091284dac948f77aed1224656b
BlocksScanIO <support@blocksscan.io>
2022-04-19 20:33:52 +0530

    Apothem MEW Dedicated RPC.

--- connections.js
+++ connections.js
@@ -27,7 +27,7 @@ module.exports = [
   'wss://nodes.mewapi.io:443',
   'wss://rpc.gochain.io:443/ws',
   'wss://mewws.blocksscan.io:443/ws',
-  'wss://apothemrpc.blocksscan.io:443/ws',
+  'wss://mewarpc.blocksscan.io:443/ws',
   'wss://www.ethercluster.com:443',
   // intercom stuff
   'api-iam.intercom.io:443',
--- src/utils/networks/nodes/apothemnetwork-ws.js
+++ src/utils/networks/nodes/apothemnetwork-ws.js
@@ -2,7 +2,7 @@ import XDC from '../types/tXDC';
 export default {
   type: XDC,
   service: 'apothem-network-ws',
-  url: 'wss://apothemrpc.blocksscan.io/ws',
+  url: 'wss://mewarpc.blocksscan.io/ws',
   port: 443,
   auth: false,
   username: '',
