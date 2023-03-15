7ef5bfdbb72266c97fdab076be03a17689f1deaf
BlocksScanIO <support@blocksscan.io>
2023-03-15 20:59:43 +0400

    update

--- src/utils/networks/nodes/apothemnetwork-ws.js
+++ src/utils/networks/nodes/apothemnetwork-ws.js
@@ -2,7 +2,7 @@ import XDC from '../types/tXDC';
 export default {
   type: XDC,
   service: 'apothem-network-ws',
-  url: 'wss://mewarpc.blocksscan.io/ws',
+  url: 'wss://mewarpc.xdcrpc.com/ws',
   port: 443,
   auth: false,
   username: '',
